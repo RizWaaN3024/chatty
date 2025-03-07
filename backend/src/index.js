feb98d7dcbf269966262b7af50bb505c1451f28e
RizWaaN3024 <rizwaanans786@gmail.com>
2025-03-08 00:44:19 +0530

    Code Fixes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 
 const app = express();
+dotenv.config();
+const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
 
-app.listen(5001, () => {
-    console.log("Server is running");
+app.listen(PORT, () => {
+    console.log("Server is running on port", PORT);
 })
