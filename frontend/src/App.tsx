b33a62449e30d718ad1026e8050b9e47ae43a3b7
RizWaaN3024 <rizwaanans786@gmail.com>
2025-03-12 01:22:06 +0530

    [setup] Page routes setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,25 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
+import HomePage from './pages/HomePage'
+import SignUpPage from './pages/SignUpPage'
+import LoginPage from './pages/LoginPage'
+import SettingsPage from './pages/SettingsPage'
+import ProfilePage from './pages/ProfilePage'
 
 function App() {
 
     return (
         <div>
             <Navbar />
-                <Routes>
-                    <Route />
-                </Routes>
+
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/signup' element={<SignUpPage />} />
+                <Route path='/login' element={<LoginPage />} />
+                <Route path='/settings' element={<SettingsPage />} />
+                <Route path='/profile' element={<ProfilePage />} />
+            </Routes>
         </div>
     )
 }
