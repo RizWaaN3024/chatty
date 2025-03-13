ee6186002aeb2b99c0ecc0635c243b9959bd057d
RizWaaN3024 <rizwaanans786@gmail.com>
2025-03-13 20:07:22 +0530

    [feature] Added loading state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,9 +6,24 @@ import SignUpPage from './pages/SignUpPage'
 import LoginPage from './pages/LoginPage'
 import SettingsPage from './pages/SettingsPage'
 import ProfilePage from './pages/ProfilePage'
+import { useAuthStore } from './store/useAuthStore.js'
+import { useEffect } from 'react'
+import { Loader } from 'lucide-react'
 
 function App() {
+    const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
 
+    useEffect(() => {
+        checkAuth();
+    }, [checkAuth]);
+
+    if (isCheckingAuth && !authUser) {
+        return (
+            <div className='flex items-center justify-center h-screen'>
+                <Loader className='size-10 animate-spin' />
+            </div>
+        )
+    }
     return (
         <div>
             <Navbar />
