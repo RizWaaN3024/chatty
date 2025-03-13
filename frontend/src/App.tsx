1f9dc741a5c822a26ceb3b2bfd5de769695484e6
RizWaaN3024 <rizwaanans786@gmail.com>
2025-03-13 20:13:08 +0530

    [router] updated routes for authenticated and non authenticated users

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Navbar from './components/Navbar'
-import HomePage from './pages/HomePage'
-import SignUpPage from './pages/SignUpPage'
-import LoginPage from './pages/LoginPage'
-import SettingsPage from './pages/SettingsPage'
-import ProfilePage from './pages/ProfilePage'
-import { useAuthStore } from './store/useAuthStore.js'
-import { useEffect } from 'react'
-import { Loader } from 'lucide-react'
+import { useEffect } from 'react';
+import './App.css';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import SignUpPage from './pages/SignUpPage';
+import LoginPage from './pages/LoginPage';
+import SettingsPage from './pages/SettingsPage';
+import ProfilePage from './pages/ProfilePage';
+import { useAuthStore } from './store/useAuthStore.js';
+import { Loader } from 'lucide-react';
 
 function App() {
     const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
@@ -29,11 +29,11 @@ function App() {
             <Navbar />
 
             <Routes>
-                <Route path='/' element={<HomePage />} />
-                <Route path='/signup' element={<SignUpPage />} />
-                <Route path='/login' element={<LoginPage />} />
+                <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+                <Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+                <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
                 <Route path='/settings' element={<SettingsPage />} />
-                <Route path='/profile' element={<ProfilePage />} />
+                <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
             </Routes>
         </div>
     )
