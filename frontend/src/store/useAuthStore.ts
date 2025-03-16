dd258ad92877a933c004e11d1bbf055ef0a6095b
RizWaaN3024 <rizwaanans786@gmail.com>
2025-03-17 01:33:57 +0530

    [auth] Updated auth store

--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
 
 export const useAuthStore = create((set) => ({
     authUser: null,
@@ -21,7 +22,14 @@ export const useAuthStore = create((set) => ({
         }
     },
 
-    signup: async ({ fullName, email, password }) => {
-
+    signup: async (data) => {
+        set({ isSigningUp: true });
+        try {
+            const res = await axiosInstance.post("/auth/signup", data);
+            set({ authUser: res.data });
+            toast.success("Account created successfully");
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
     }
 }));
