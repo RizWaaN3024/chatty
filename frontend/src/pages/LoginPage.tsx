7206f1b181f554f146ca899e3c13b903ad0b3c9c
RizWaaN3024 <rizwaanans786@gmail.com>
2025-03-20 00:45:55 +0530

    [page] Login Page

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAuthStore } from '../store/useAuthStore';
 
 const LoginPage = () => {
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+    });
+    const { login, isLoggingIn } = useAuthStore();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        login(formData);
+    }
     return (
         <div>
 
