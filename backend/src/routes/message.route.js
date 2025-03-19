5e4b8cc9a3f5031fbcc06d03e434f05a4c4a3e12
RizWaaN3024 <rizwaanans786@gmail.com>
2025-03-20 00:17:13 +0530

    [API] Code fixes

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -2,7 +2,7 @@ import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
 import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
 
-const router = expresss.Router();
+const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
 router.get("/:id", protectRoute, getMessages);
