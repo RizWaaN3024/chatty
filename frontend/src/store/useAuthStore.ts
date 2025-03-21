1b33838357d15a00886863f58e08a55c9dcf97a8
RizWaaN3024 <rizwaanans786@gmail.com>
2025-03-22 01:56:19 +0530

    [store] update profile functionality

--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -59,6 +59,16 @@ export const useAuthStore = create((set) => ({
     },
 
     updateProfile: async (data) => {
-        
+        set({ isUpdatingProfile: true });
+        try {
+            const res = await axiosInstance.put("/auth/update-profile", data);
+            set({ authUser: res.data });
+            toast.success("Profile updated successsfully");
+        } catch (error) {
+            console.log("Error in update profile", error);
+            toast.error(error.response.data.message);
+        } finally {
+            set({ isUpdatingProfile: false });
+        }
     }
 }));
