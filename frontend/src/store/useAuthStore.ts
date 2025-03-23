79de40164ccac2d2d469a283812a6d41ef42c2fd
RizWaaN3024 <rizwaanans786@gmail.com>
2025-03-23 23:35:02 +0530

    code fixes

--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -28,7 +28,7 @@ export const useAuthStore = create((set) => ({
             const res = await axiosInstance.post("/auth/signup", data);
             set({ authUser: res.data });
             toast.success("Account created successfully");
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
         } catch (error: any) {
             toast.error(error.response.data.message);
         } finally {
