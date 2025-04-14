f57231d51aabbe7a5b569637770a929141f5259a
RizWaaN3024 <rizwaanans786@gmail.com>
2025-04-14 20:16:05 +0530

    [component] Sidebar component

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useChatStore } from '../store/useChatStore'
 
 const Sidebar = () => {
+    const { getUsers, users, selectedUser, setSelectedUser, isUserLoading } = useChatStore();
+
+    const onlineUsers = [];
+
+    useEffect(() => {
+        getUsers();
+    }, [getUsers])
     return (
         <div>
 
