bb098de112581daa3efdc90022b5f42e4cd1f14d
RizWaaN3024 <rizwaanans786@gmail.com>
2025-04-14 20:43:46 +0530

    [component] Chat container initial

--- frontend/src/components/ChatContainer.tsx
+++ frontend/src/components/ChatContainer.tsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useChatStore } from '../store/useChatStore'
 
 const ChatContainer = () => {
+    const { messages, getMessages, isMessagesLoading, selectedUser } = useChatStore();
+
+    useEffect(() => {
+        getMessages(selectedUser._id);
+    }, [selectedUser._id, getMessages]);
+
+    if (isMessagesLoading) return <div>Loading...</div>
+    
     return (
         <div>
 
