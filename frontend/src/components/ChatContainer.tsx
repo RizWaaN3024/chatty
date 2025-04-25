9d84a7e25ec96bede223631b57995e4cc4def38b
RizWaaN3024 <rizwaanans786@gmail.com>
2025-04-25 23:57:28 +0530

    code fixes

--- frontend/src/components/ChatContainer.tsx
+++ frontend/src/components/ChatContainer.tsx
@@ -11,8 +11,7 @@ const ChatContainer = () => {
         getMessages(selectedUser._id);
     }, [selectedUser._id, getMessages]);
 
-    if (isMessagesLoading) 
-    {
+    if (isMessagesLoading) {
         return (
             <div className="flex-1 flex flex-col overflow-auto">
                 <ChatHeader />
