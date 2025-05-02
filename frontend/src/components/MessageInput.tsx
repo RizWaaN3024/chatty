f25a1fcc2bb085dc0bc05a393c5cf526dab871c6
RizWaaN3024 <rizwaanans786@gmail.com>
2025-05-03 01:14:33 +0530

    code fixes

--- frontend/src/components/MessageInput.tsx
+++ frontend/src/components/MessageInput.tsx
@@ -60,6 +60,7 @@ const MessageInput = () => {
                             className="w-20 h-20 object-cover rounded-lg border border-zinc-700"
                         />
                         <button
+                        onClick={removeImage}
                             className="absolute -top-1.5 -right-1.5 w-5 h-5 rounded-full bg-base-300 flex items-center justify-center"
                             type="button"
                         >
@@ -69,7 +70,7 @@ const MessageInput = () => {
                 </div>
             )}
 
-            <form className="flex items-center gap-2">
+            <form onSubmit={handleSendMessage} className="flex items-center gap-2">
                 <div className="flex-1 flex gap-2">
                     <input
                         type="text"
--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -20,7 +20,7 @@ export const useAuthStore = create((set, get) => ({
             const res = await axiosInstance.get("/auth/check");
 
             set({ authUser: res.data });
-            get.connectSocket();
+            get().connectSocket();
         } catch (error) {
             console.log("Error in check auth", error);
             set({ authUser: null });
